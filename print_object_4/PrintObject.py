8b91129544935a5f42933f54a756c79aa9641720
Olesya Ivanova <iod5000@gmail.com>
2022-01-22 03:02:19 +0800

    task 4 done

---
+++ print_object_4/PrintObject.py
@@ -0,0 +1,32 @@
+class A:
+    def __init__(self, value: int):
+        self.value = value
+
+    # def __str__(self):
+    #     return str(self.value)
+
+
+class B:
+    def __init__(self, value: float):
+        self.value = value
+
+
+class ObjectException(Exception):
+    pass
+
+
+def func(obj) -> None:
+    class_name = str(obj.__class__)
+    if class_name.__contains__('B'):
+        raise ObjectException("You entered class B object")
+    elif class_name.__contains__('A'):
+        print(obj.value)
+    else:
+        print(obj)
+
+
+if __name__ == '__main__':
+    func(A(10))
+    func(B(1.5))
+    func("hello!")
+
---
+++ print_object_4/__init__.py
@@ -0,0 +1,1 @@
+from .PrintObject import func, A, B, ObjectException
